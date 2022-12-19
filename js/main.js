bb7278cbd08b710f239c64808d76ff4128ce651a
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-12-20 04:04:26 +0500

    Add simple functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const elForm = document.querySelector(".form"); // ✅
 const elInput = document.querySelector(".form-control"); // ✅
-const elResult = document.querySelector(".result");
-const elAlertError = document.querySelector(".alert");
+const elResult = document.querySelector(".result"); // ✅
+const elAlertError = document.querySelector(".alert"); // ✅
+const elClearBtn = document.querySelector(".clear"); // ✅
 
 function msg() {
   elForm.addEventListener("submit", function (evt) {
+    //* Prevents the page from refreshing
     evt.preventDefault();
+
+    //* Checks if the input is valid
     if (elInput.value === "") {
       setTimeout(() => {
         elAlertError.classList.remove("d-none");
@@ -18,8 +22,11 @@ function msg() {
 
     elResult.textContent = elInput.value;
     elInput.value = "";
+  });
 
-    //   Checks if the input is valid
+  //* Removes the texts that were sent
+  elClearBtn.addEventListener("click", function () {
+    elResult.textContent = "";
   });
 }
 msg();
